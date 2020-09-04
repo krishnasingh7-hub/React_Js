5b1e53db5ba3e26f29aa336f729c196f8f2a460f
krishna singh <k7.krishna5@gmail.com>
2020-09-04 20:45:52 +0530

    React Assignment 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component{
+
+	constructor(props){
+		super(props)
+		this.state = {
+			
+		}
+	}
+
+	renderComments(showcomment){
+		console.log(showcomment)
+		const month = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sep','Oct','Nov','Dec']
+		const x = showcomment.map((cmnt)=>{
+					return(
+						<ul className="list-unstyled" key={cmnt.id}>
+							
+							<li>{cmnt.comment}</li>
+							<li> -- {cmnt.author} , {month[parseInt(cmnt.date.slice(5,7))-1]}  {("0"+(parseInt(cmnt.date.slice(8,10))+1).toString()).slice(-2)}, {cmnt.date.slice(0,4)} </li>
+
+						</ul>
+					);
+				})
+		return(
+			<div>
+				<h4>Comments</h4>
+				<div>
+					{x}
+				</div>
+			</div>
+			);
+	}
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+				<div className="row">
+				<div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                	{this.renderComments(dish.comments)}
+                </div>
+                </div>
+
+				
+
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+
+	render(){
+
+		return(
+			<div className="container">
+
+				{this.renderDish(this.props.dishshow)}
+				
+			</div> 
+		);
+	}
+}
+
+export default DishDetail;
