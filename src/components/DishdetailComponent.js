1911798dfa44ccd393819aa614894af0d0c466c0
krishna singh <k7.krishna5@gmail.com>
2021-01-07 22:11:21 +0530

    React Animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required =(val) =>val&&val.length;
 const maxLength = (len)=>(val)=>!(val)||(val.length<=len);
@@ -123,13 +124,19 @@ class CommentForm extends Component{
             return(
 				
 				<div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={baseUrl + Dish.image} alt={Dish.name} />
-                    <CardBody>
-                      <CardTitle>{Dish.name}</CardTitle>
-                      <CardText>{Dish.description}</CardText>
-                    </CardBody>
-                </Card>
+					<FadeTransform
+						in
+						transformProps={{
+							exitTransform: 'scale(0.5) translateY(-50%)'
+						}}>
+						<Card>
+							<CardImg top src={baseUrl + Dish.image} alt={Dish.name} />
+							<CardBody>
+							<CardTitle>{Dish.name}</CardTitle>
+							<CardText>{Dish.description}</CardText>
+							</CardBody>
+						</Card>
+					</FadeTransform>
                 </div>
 
             );
